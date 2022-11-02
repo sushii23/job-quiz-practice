fee1bb1c572cbcd4abd585f3fb76371c13987cf3
Adrian Deon <sushdawg@Adrians-MacBook-Pro.local>
2022-11-02 15:19:56 -0500

    added css

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,7 @@
-const startb = document.querySelector("#startb");
-const timerDiv = document.querySelector("timer");
-const questionDiv =document.querySelector("questions");
+const startb = document.querySelector(".startb");
+const timerDiv = document.querySelector(".timer");
+const timeSpan = document.querySelector(".time");
+const questionsDiv = document.querySelector("questions");
 
 
 const questions = [
@@ -29,5 +30,61 @@ const questions = [
 
 let questionIndex = 0;
 let time =60;
+let quizTimer;
 
-const startQuiz = () =>
+const startQuiz = () => {
+      quizTimer = setInterval(function() {  
+           time--
+           timeSpan.textContent = time;
+           if (time === 0) {
+               endQuiz();
+        }
+      },1000);
+
+      displayQuestion();
+};
+
+const displayQuestion = () => {
+      questionsDiv.innerHTML = "";
+      const questionText = questions[questionIndex].Text;
+      const questionAns = questions[questionIndex].answer;
+      const questionTextDiv = document.createElement("div");
+      questionTextDiv.innerText = questionText;
+      questionsDiv.append(questionTextDiv);
+
+      const questionChoices = questions[questionIndex].choices;
+      questionChoices.forEach(choice => {
+           const questionBtn = document.createElement("button");
+           questionBtn. textContent = choice;
+           questionsDiv.append(questionBtn)
+           
+           questionBtn.addEventListener("click", function(event){
+              if (event.target.textContent === questionAns) {
+                handleCorrectAns();
+              } else {
+                 handleIncorrectAns();
+              };
+           })
+      });
+      
+};
+
+const handleCorrectAns = () => {
+     questionIndex++;
+     displayQuestion();
+     console.log("correct stuff")
+};
+
+const handleIncorrectAns = () => {
+     questionIndex++;
+     displayQuestion();
+     time = time -5;
+     console.log("incorrect stuff")
+};
+
+const endQuiz = () => {
+    clearInterval(quizTimer);
+}
+
+
+startb.addEventListener("click", startQuiz);
