a0e9783fdd9a5369d7071cb2dc72bd4bc1fa7258
Adrian Deon <sushdawg@Adrians-MacBook-Pro.local>
2022-12-05 07:51:35 -0600

    added start function

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,7 +4,25 @@ var time = questions. length * 15;
 var timerID;
 
 // get the elements out of the html by Id//
-var questionsEL = document
+var questionsEL = document.getElementById("questions");
+var timerEL = document.getElementById("time");
+var start_btn = document.getElementById("start");
+var choicesEL = document.getElementById("choices");
+var initialsEL = document.getElementById("initials");
+var submit_btn = document.getElementById("submit");
+var feedbackEL = document.getElementById("feedback");
+
+
+function startQuiz() {
+// get rid of start screen//
+  var firstScreenEl= document.getElementById("startScreen");
+  firstScreenEl.setAttribute("class", "hide");
+
+
+
+
+
+}
 
 
 
