8ca2fc544c61d0915e481aa84fc6416f20f0c63c
Adrian Deon <sushdawg@Adrians-MacBook-Pro.local>
2022-12-05 08:16:26 -0600

    added get question function

--- assets/js/script.js
+++ assets/js/script.js
@@ -14,15 +14,36 @@ var feedbackEL = document.getElementById("feedback");
 
 
 function startQuiz() {
-// get rid of start screen//
+// get rid of start screen, set attribute to change from showing to hiding//
   var firstScreenEl= document.getElementById("startScreen");
   firstScreenEl.setAttribute("class", "hide");
+
+  // remove the hiding attribute to show questions//
+  questionsEL.removeAttribute("class");
+
+// awakes the timer,sets attributes//
+timerID = setInterval(clockTick, 1000);
+
+// show starting time//
+timerEL.textContent = time;
+
+getQuestion();
+}
+
+function getQuestion() {
+// get the present question//
+var presentQuestion = questions[presentQuestionIndex];
+// update title each time there's a new question//
+var titleEl = document.getElementById("questionTitle");
+titleEl.textContent = presentQuestion.title;
+
+
+
 
 
 
 
 
 }
-
 
 
