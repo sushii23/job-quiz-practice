502f48131b6e7763e841a3de5e92d0a7676c90c4
Adrian Deon <sushdawg@Adrians-MacBook-Pro.local>
2022-12-05 07:29:30 -0600

    added script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,10 @@
+// variables to set the foundation//
+var presentQuestionIndex = 0;
+var time = questions. length * 15;
+var timerID;
+
+// get the elements out of the html by Id//
+var questionsEL = document
+
+
 
